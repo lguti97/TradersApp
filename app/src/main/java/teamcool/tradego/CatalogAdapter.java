a697f4f03102b6dd8b828582ec1e25b84cc05bd3
Selina Bing <selinabing@fb.com>
2016-07-06 15:41:17 -0700

    added adapter

---
+++ app/src/main/java/teamcool/tradego/CatalogAdapter.java
@@ -0,0 +1,64 @@
+package teamcool.tradego;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by selinabing on 7/6/16.
+ */
+public class CatalogAdapter extends RecyclerView.Adapter<CatalogAdapter.ViewHolder> {
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        //butterknife binding goes here:
+        //@BindView(R.id.someId) Type someId;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this,itemView);
+
+            //set item's onClickListener to (this);
+        }
+    }
+
+    private List<Item> items;
+
+    public CatalogAdapter(List<Item> items) {
+        this.items = items;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View catalogView = inflater.inflate(R.layout.layout_here, parent, false);
+        ViewHolder viewHolder = new ViewHolder(catalogView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Item item = items.get(position);
+        //populate each item by setting its text and media
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public void clear() {
+        items.clear();
+        notifyDataSetChanged();
+    }
+
+    public void clearAndAddAll(List<Item> newItems) {
+        items.addAll(items);
+        notifyDataSetChanged();
+    }
+}
