7818ead87a621994dfe620f371b8b6aead55b9c2
Luis Gutierrez <lguti@fb.com>
2016-07-07 16:28:37 -0700

    Parse server works and I tested it

--- app/src/main/java/teamcool/tradego/MainActivity.java
+++ app/src/main/java/teamcool/tradego/MainActivity.java
@@ -2,31 +2,41 @@ package teamcool.tradego;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String YOUR_APPLICATION_ID = "tradego";
-    public static final String YOUR_CLIENT_KEY = "1997";
+    //public static final String YOUR_CLIENT_KEY = "1997";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         /*
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, YOUR_APPLICATION_ID, YOUR_CLIENT_KEY); */
 
+
         Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
-                .applicationId(YOUR_APPLICATION_ID).clientKey(YOUR_CLIENT_KEY)
-                .server("http://tradego.herokuapp:1337/parse").build());
+                .applicationId(YOUR_APPLICATION_ID)
+                .server("http://tradego.herokuapp.com/parse/").build());
 
         ParseObject testObject = new ParseObject("TestObject");
         testObject.put("foo", "bar");
         testObject.saveInBackground();
 
+
+        //Toast.makeText(this, testObject.get("foo"), Toast.LENGTH_SHORT).show(); */
+
     }
+
 }
