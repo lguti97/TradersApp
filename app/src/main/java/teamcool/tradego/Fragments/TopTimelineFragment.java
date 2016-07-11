fc13169f4fd437a12be0a1bf2ab746ecea1d2917
Selina Bing <selinabing@fb.com>
2016-07-10 19:39:00 -0700

    minor changes

--- app/src/main/java/teamcool/tradego/Fragments/TopTimelineFragment.java
+++ app/src/main/java/teamcool/tradego/Fragments/TopTimelineFragment.java
@@ -17,7 +17,7 @@ public class TopTimelineFragment extends CatalogListFragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //populate time line
+        populateTimeLine();
     }
 
     @Nullable
@@ -34,6 +34,8 @@ public class TopTimelineFragment extends CatalogListFragment {
         super.onViewCreated(view, savedInstanceState);
     }
 
-    //method to populate timeline
+    public void populateTimeLine() {
+        return; //placeholder
+    }
 
 }
