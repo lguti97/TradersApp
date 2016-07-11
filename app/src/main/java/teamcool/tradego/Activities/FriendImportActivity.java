cb1a201b5ad92401d18d94d681c5f5c5c0328e14
Luis Gutierrez <lguti@fb.com>
2016-07-11 10:17:11 -0700

    nothing

--- app/src/main/java/teamcool/tradego/Activities/FriendImportActivity.java
+++ app/src/main/java/teamcool/tradego/Activities/FriendImportActivity.java
@@ -3,6 +3,11 @@ package teamcool.tradego.Activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+
+import org.json.JSONObject;
+
 import teamcool.tradego.R;
 
 /*
@@ -15,6 +20,21 @@ public class FriendImportActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend_import);
+
+        GraphRequest request = GraphRequest.newMeRequest(
+                accessToken,
+                new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(
+                            JSONObject object,
+                            GraphResponse response) {
+                        // Application code
+                    }
+                });
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "id,name,link");
+        request.setParameters(parameters);
+        request.executeAsync();
 
     }
 
