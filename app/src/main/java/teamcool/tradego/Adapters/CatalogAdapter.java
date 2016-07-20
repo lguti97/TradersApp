d6db0a2bd0e68d6ead5d82de5391ef6119deb2ef
Selina Bing <selinabing@fb.com>
2016-07-20 09:55:37 -0700

    minor changes to adapter

--- app/src/main/java/teamcool/tradego/Adapters/CatalogAdapter.java
+++ app/src/main/java/teamcool/tradego/Adapters/CatalogAdapter.java
@@ -97,6 +97,7 @@ public class CatalogAdapter extends RecyclerView.Adapter<CatalogAdapter.ViewHold
     }
 
     public void clearAndAddAll(List<Item> newItems) {
+        items.clear();
         items.addAll(newItems);
         notifyDataSetChanged();
     }
