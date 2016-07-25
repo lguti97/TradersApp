81cc1c16d6631eabb1c800dc988dec68d7ace5a6
Kinjal Shah <kshah97@fb.com>
2016-07-25 11:18:39 -0700

    trying different approaches to bitmap

--- app/src/main/java/teamcool/tradego/Activities/LoginActivity.java
+++ app/src/main/java/teamcool/tradego/Activities/LoginActivity.java
@@ -122,7 +122,7 @@ public class LoginActivity extends AppCompatActivity {
                 // it might be because you have3 no PERMISSION to access it
                 // look it up in API and add more permissions to the array
 
-                Intent i = new Intent (LoginActivity.this, NewsFeedActivity.class);
+                Intent i = new Intent (LoginActivity.this, FriendImportActivity.class);
 
                 startActivity(i);
             }
