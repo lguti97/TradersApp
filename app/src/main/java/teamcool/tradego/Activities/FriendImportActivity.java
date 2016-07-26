a684b0f1a507aea67a2329f95c30787058cdfb6a
Luis Gutierrez <lguti@fb.com>
2016-07-26 12:06:37 -0700

    made modifcations to FriendImport

--- app/src/main/java/teamcool/tradego/Activities/FriendImportActivity.java
+++ app/src/main/java/teamcool/tradego/Activities/FriendImportActivity.java
@@ -1,6 +1,7 @@
 package teamcool.tradego.Activities;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -25,6 +26,7 @@ import java.util.HashMap;
 import teamcool.tradego.Adapters.AcquaintanceAdapter;
 import teamcool.tradego.Clients.FBGraphClient;
 import teamcool.tradego.Clients.ParseClient;
+import teamcool.tradego.Fragments.AlertSwipeFragment;
 import teamcool.tradego.Models.Acquaintance;
 import teamcool.tradego.R;
 import teamcool.tradego.SimpleItemTouchHelperCallback;
@@ -43,10 +45,9 @@ public class FriendImportActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend_import);
-        //Doing some channel testing here. Why is this not working?
-        HashMap<String, String> test = new HashMap<>();
-        test.put("channel", "testing");
-        ParseCloud.callFunctionInBackground("pushChannelTest", test);
+        //Show Alert Dialog Fragment
+        showAlertDialog();
+
 
         //This is where I make the acquaintance objects.
         final ArrayList<Acquaintance> acquaintances = new ArrayList<>();
@@ -81,6 +82,9 @@ public class FriendImportActivity extends AppCompatActivity {
         overridePendingTransition(R.anim.right_in, R.anim.left_out);
     }
 
-    //Creates the ParseObject Acquaintance + makes sure it's not created again for the current ParseUser
+    public void showAlertDialog() {
+        AlertSwipeFragment alertDialog = AlertSwipeFragment.newInstance("Welcome!");
+        alertDialog.show(getFragmentManager(), "fragment_alert");
+    }
 
 }
